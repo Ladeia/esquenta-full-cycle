78c86cd770a4bdea16a4c8cde8579844ba033905
ladeia <contato@antonioladeia.com>
2024-06-13 10:24:50 -0300

    add interface example in struct

--- struct.go
+++ struct.go
@@ -2,6 +2,10 @@ package main
 
 import "fmt"
 
+type Veiculo interface {
+	andar()
+}
+
 type Carro struct {
 	modelo string
 	ano    int
@@ -11,10 +15,15 @@ func (c Carro) andar() {
 	fmt.Println("O carro", c.modelo, "está andando")
 }
 
+func vemAndarComigo(v Veiculo) {
+	v.andar()
+}
+
 func main() {
 	carro1 := Carro{modelo: "March", ano: 2014}
 	//carro2 := Carro{modelo: "March", ano: 2014}
 
 	fmt.Println(carro1.modelo, carro1.ano)
 	carro1.andar()
+	vemAndarComigo(carro1)
 }
